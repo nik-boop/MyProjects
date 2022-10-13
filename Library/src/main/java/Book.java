45ac97b4f7f19612080b68be0e67faeac6d454c1
Nikita <nakoryakin02@gmail.com>
2022-10-13 18:51:52 +0300

    Update documentation

--- Library/src/main/java/Book.java
+++ Library/src/main/java/Book.java
@@ -8,15 +8,15 @@ import java.util.HashMap;
 public class Book {
 
     /**
-     *Путь от корневой директории до папки с ресурсамти
+     * Путь от корневой директории до папки с ресурсами
      */
     private final String rootPath = "src/main/resources";
     /**
-     *Папка с файлаами атрибутов книг
+     * Папка с файлами атрибутов книг
      */
     private final String dataDir = "Books";
     /**
-     * Имена фалвов хранящих информацию о книге
+     * Имена файлов хранящих информацию о книге
      */
     private final String[] columnsName  = new String[] {"id", "name", "author", "publisher", "edition", "publication_year", "category"};
 
@@ -33,25 +33,29 @@ public class Book {
      */
     private String readColumnName;
     /**
+     * Ридер для файла со значениями атрибутов книги
      * {@code java.io.FileReader}
+     * Для создания используется функция {@link Book#startRead(String) startRead}
      */
     private java.io.FileReader reader;
 
     /**
-     *  {@code ArrayList} хранящий HashMap<String, String> с записями атрибутов всех книг
+     *  {@code ArrayList} хранящий {@code HashMap<String, String>} с записями атрибутов всех книг
+     *  Можно получить через геттер {@link Book#getAllDataList()}}
      */
     private ArrayList<HashMap<String, String>> allData;
 
     /**
      * ID всех книг
-     * После создания экземпляра нужно обновить
+     * После создания экземпляра обновляется
+     * Можно обновить через метод {@link Book#getIdBooks()}
      */
     private ArrayList<Integer> IDBooks = new ArrayList<>();
 
     /**
-     * Creator класса
-     * при создании экземпляра считывает всю информацию в переменную addData
-     * и получает ID книг
+     * Creator класса {@link Book }
+     * при создании экземпляра считывает всю информацию в переменную {@code allData}
+     * и получает ID книг {@link Book#IDBooks}
      */
     public Book() {
         try {
@@ -64,9 +68,9 @@ public class Book {
     }
 
     /**
-     * Проверка есть ли у книиги заданный атрибут
+     * Проверка есть ли у книги заданный {@link Book#columnsName атрибут}
      * @param column имя атрибута
-     * @return true ели токой атрибут есть false если нет
+     * @return {@code true} ели такой атрибут есть {@code false} если нет
      */
     private boolean checkColumnName(String column){
         if (column.equals("log")) return true;
@@ -80,7 +84,7 @@ public class Book {
 
     /**
      * Считает количество строк и записывает в переменную {@code length}
-     * @throws Exception В случае если неполучается открыть файл вызывет исключение
+     * @throws Exception В случае если не получается открыть файл вызывает исключение
      */
     public void getCountRows() throws Exception {
         startRead(columnsName[0]);
@@ -92,9 +96,10 @@ public class Book {
     }
 
     /**
-     * Получает ID сех книг
-     * @return Возвращает {@code ArrayList<Integer>} с ID сех книг
-     * @throws Exception В случае если неполучается открыть файл вызывет исключение
+     * Получает ID всех книг.
+     * После чего перезаписывает атрибут класса {@link Book#IDBooks IDBooks}
+     * @return Возвращает {@code ArrayList<Integer>} с ID всех книг
+     * @throws Exception В случае если не получается открыть файл вызывает исключение
      */
     public ArrayList<Integer> getIdBooks() throws Exception {
         startRead("id");
@@ -108,9 +113,11 @@ public class Book {
     }
 
     /**
-     * Начинат считывание файла с атрибутом книги
+     * Начинать считывание файла с {@link Book#columnsName атрибутом} книги.
+     * При начале считывания все изменяемые переменные с используемые для чтения ({@link Book#readColumnName читаемый атрибут},
+     * {@link Book#Index индекс текущей строки}) сбрасываются, а {@link Book#reader } заменяется на новый
      * @param column имя атрибута для считывания
-     * @throws Exception В случае если неполучается открыть файл вызывет исключение
+     * @throws Exception В случае если не получается открыть файл вызывает исключение
      */
     private void startRead(String column) throws Exception {
         // Проверка на существование заданного атрибута
@@ -132,8 +139,8 @@ public class Book {
     /**
      * Внутренний метод для перезаписи атрибута всех книг
      * используется для удаления строк
-     * @param column атрибут
-     * @param data содержание файла - удаляемая строка
+     * @param column {@link Book#columnsName атрибут}
+     * @param data содержание файла минус удаляемая строка
      */
     private void rewriteColumn(String column, String data){
         try {
@@ -147,14 +154,14 @@ public class Book {
     }
 
     /**
-     * Добавление в атрибут нового занчения
-     * @param column атрибут
-     * @param row значение
+     * Добавление в атрибут нового значения
+     * @param column {@link Book#columnsName атрибут}
+     * @param value значение
      */
-    private void addToColumn(String column, String row) {
+    private void addToColumn(String column, String value) {
         try {
             // Дозапись в существующий файл нового значения
-            Files.write(Path.of(rootPath + "/" + dataDir + "/" + column), row.getBytes(), StandardOpenOption.APPEND);
+            Files.write(Path.of(rootPath + "/" + dataDir + "/" + column), value.getBytes(), StandardOpenOption.APPEND);
         }catch (IOException e) {
             System.out.println("Text append failed.");
             e.printStackTrace();
@@ -162,17 +169,17 @@ public class Book {
     }
 
     /**
-     * Функция для перехода по строкам атрибутов книг
-     * @return Идекс текущей строки, если файл кончиля вернется -1
+     * Функция для перехода по строкам {@link Book#columnsName атрибутов} книги
+     * @return {@link Book#Index Индекс} текущей строки, если файл закончился, вернется -1
      */
     private int stepOnNextLine(){
-        // переменнаяя для записи считываемого символа
+        // переменная для записи считываемого символа
         int character = 0;
         try {
             character = reader.read();
-            // пока в файле есть симвлы считываем их
+            // пока в файле есть символы считываем их
             while (character != -1) {
-                // Если считаный символ это переход на повую строку то считывание превывается
+                // Если считанный символ это переход на новую строку, то считывание прерывается
                 // и возвращается индекс новой строки
                 if (character == 10){
                     Index++;
@@ -184,32 +191,32 @@ public class Book {
             System.out.println("Text reading failed.");
             e.printStackTrace();
         }
-        // если файл кончиля вернется -1
+        // если файл кончился, вернется -1
         return -1;
     }
 
     /**
-     * Функция для считывания текущей строки читаемого файла.
+     * Функция для считывания текущей строки {@link Book#reader читаемого файла}.
      * После считывания переходит на новую строку
-     * @return Значение в строкке, если ничего считать не удалось — файл кончиля, вернется null
+     * @return Значение в строке, если ничего считать не удалось — файл кончился, вернется {@code null}
      */
     private String getLine(){
-        // переменнаяя для записи считываемого символа
+        // переменная для записи считываемого символа
         int character = 0;
         // StringBuilder для сохранения прочтенных символов
         StringBuilder sb = new StringBuilder();
         try {
             // Считывание символа
             character = reader.read();
-            // пока в файле есть симвлы считываем их
+            // пока в файле есть символы считываем их
             while (character != -1) {
                 if (character == 10){
-                    // Если считаный символ это переход на повую строку то считывание превывается
+                    // Если считанный символ это переход на новую строку, то считывание прерывается
                     // и возвращается значение считанной строки
                     Index++;
                     return sb.toString();
                 }
-                // Схраняем считаный символ
+                // Сохраняем считанный символ
                 sb.append((char) character);
                 character = reader.read();
             }
@@ -217,15 +224,15 @@ public class Book {
             System.out.println("Text reading failed.");
             e.printStackTrace();
         }
-        // если ничего считать не удалось — файл кончиля, вернется null
+        // если ничего считать не удалось — файл кончился, вернется null
         return null;
     }
 
     /**
-     * функция для получения значения строки с аданым индексом
+     * Функция для получения значения строки с заданным индексом
      * @param index Индекс считываемой строки
      * @return значение в считываемой строке
-     * @throws Exception Если индекс запрашиваемой строки менньше текущего то вызывется исключение
+     * @throws Exception Если индекс запрашиваемой строки меньше текущего, то вызывается исключение
      * т.к прочесть значение строки будет невозможно
      */
     private String getValueFromIndex(int index) throws Exception {
@@ -237,10 +244,10 @@ public class Book {
     }
 
     /**
-     * Функция проверяет есть ли у книги заданный атрибут и возвращает Индексы всех книг с таким атрибутом
-     * @param column атрибут книги
+     * Функция проверяет есть ли у книги заданный {@link Book#columnsName атрибут} и возвращает Индексы всех книг с таким атрибутом
+     * @param column {@link Book#columnsName атрибут} книги
      * @param value искомое значение
-     * @return {@code ArrayList<Integer>} с Индексами! строк где содержится информация о книге
+     * @return {@code ArrayList<Integer>} с Индексами! строк, где содержится информация о книге
      */
     public ArrayList<Integer> getIndexesFromValue(String column, String value){
         try {
@@ -250,8 +257,8 @@ public class Book {
         }
         ArrayList<Integer> id = new ArrayList<>();
         String getValue = getLine();
-        // Проходим по всем значениям атрибутов и ищем соотвветствия,
-        // если соответствие найдено добавляем индекс к списку отввета
+        // Проходим по всем значениям атрибутов и ищем соответствия,
+        // если соответствие найдено добавляем индекс к списку ответа
         while (getValue != null) {
             if (getValue.equals(value)){
                 id.add(Index -1);
@@ -262,27 +269,27 @@ public class Book {
     }
 
     /**
-     * Функция для получения всей инфрмации о книге по индексу
-     * @param index индекс получемой строки
-     * @return {@code HashMap<String, String>} где key это атрибут а value это значение атрибута
-     * @throws Exception В случае если неполучается открыть файл вызывет исключение
+     * Функция для получения всей информации о книге по индексу
+     * @param index индекс получаемой строки
+     * @return {@code HashMap<String, String>} где key это {@link Book#columnsName атрибут} а value это значение атрибута
+     * @throws Exception В случае если не получается открыть файл вызывает исключение
      */
     public HashMap<String, String> getRowFromIndex(int index) throws Exception {
         HashMap<String, String> bookInfo = new HashMap<>();
         // Обходим все атрибуты книги
         for (String column :columnsName){
             startRead(column);
-            // Добавляем ативибу и значение этого атрибута
+            // Добавляем атрибут и значение этого атрибута
             bookInfo.put(column, getValueFromIndex(index));
         }
         return bookInfo;
     }
 
     /**
-     * Функция для получения всей инфрмации о книгах по индексам
+     * Функция для получения всей информации о книгах по индексам
      * @param Indexes Индексы искомых книг
-     * @return {@code ArrayList<HashMap<String, String>} где key это атрибут а value это значение атрибута
-     * @throws Exception В случае если неполучается открыть файл вызывет исключение
+     * @return {@code ArrayList<HashMap<String, String>} где key это {@link Book#columnsName атрибут} а value это значение атрибута
+     * @throws Exception В случае если не получается открыть файл вызывает исключение
      */
     public ArrayList<HashMap<String, String>> getRowsFromIndex(ArrayList<Integer> Indexes) throws Exception {
         ArrayList<HashMap<String, String>> books = new ArrayList<>();
@@ -295,8 +302,9 @@ public class Book {
 
     /**
      * Функция для получения всей информации о книгах в виде {@code ArrayList<HashMap<String, String>>}
-     * @return массив с словарями информации о книгах
-     * @throws Exception В случае если неполучается открыть файл вызывет исключение
+     * записывает информацию в {@link Book#allData} с последующим доступом к ней через {@link Book#getAllDataList()}  геттер для атрибута} - {@link Book#allData}
+     * @return массив со словарями информации о книгах
+     * @throws Exception В случае если не получается открыть файл вызывает исключение
      */
     public ArrayList<HashMap<String, String>> getAllData() throws Exception {
         getCountRows();
@@ -309,11 +317,11 @@ public class Book {
 
     /**
      * Дополнительная функция для {@linkplain Book#deleteRow(int) deleteRow(int index)}
-     * удаляет запись о книге по индексу из строки с заданым атрибутом
+     * удаляет запись о книге по индексу из строки с заданным атрибутом
      * @param column атрибут
      * @param index индекс строки
      * @return возвращает удаленное значение
-     * @throws Exception В случае если неполучается открыть файл вызывет исключение
+     * @throws Exception В случае если не получается открыть файл вызывает исключение
      */
     private String deleteRowFromColumn(String column, int index) throws Exception {
         StringBuilder saveData = new StringBuilder();
@@ -328,7 +336,7 @@ public class Book {
         }
         // получение значения по индексу
         deleteValue = getLine();
-        // сичтывание всех значений после индекса
+        // считывание всех значений после индекса
         while (Index < length) {
             saveData.append(getLine()).append("\n");
         }
@@ -340,8 +348,8 @@ public class Book {
     /**
      * Удаляет информацию о книге по индексу
      * @param index индекс
-     * @return информация о удаленной книге
-     * @throws Exception В случае если неполучается открыть файл вызывет исключение
+     * @return информация об удаленной книге
+     * @throws Exception В случае если не получается открыть файл вызывает исключение
      */
     public HashMap<String, String> deleteRow(int index) throws Exception {
         getCountRows();
@@ -356,9 +364,9 @@ public class Book {
 
     /**
      * Удаляет информацию о книге по ID книги
-     * @param id Уникальныйидентификатор книги
+     * @param id Уникальный идентификатор книги
      * @return Удаляемая информация
-     * @throws Exception В случае если неполучается открыть файл вызывет исключение.
+     * @throws Exception В случае если не получается открыть файл вызывает исключение.
      * Если нет книги с таким ID
      */
     public HashMap<String, String> deleteRowByID(int id) throws Exception {
@@ -369,9 +377,9 @@ public class Book {
 
     /**
      * Добавляет запись о новой книге
-     * @param newRow информация о добаляемой книге. Имена атрибутов можно узнать через {@link Book#getColumnsName() имена атрибутов книги}
+     * @param newRow Информация о добавляемой книге. Имена атрибутов можно узнать через {@link Book#getColumnsName() имена атрибутов книги}
      * @throws Exception Если ID уже существует вызовет исключение. Все ID можно узнать через {@link Book#getIdBooks() getIdBooks() с перебором всех ID}
-     * или {@link Book#getIDBooks() getIDBooks() через атрибут экземпляра}
+     * или {@link Book#getIDBooks() getIDBooks() через атрибут экземпляра} - {@link Book#IDBooks IDBooks}
      */
     public void addNewBook(HashMap<String, String> newRow) throws Exception {
         // Проверка на уникальность ID
@@ -386,19 +394,20 @@ public class Book {
     /**
      * Добавляет запись о новой книге
      * @param id Уникальный идентификатор книги. Все ID можно узнать через {@link Book#getIdBooks() getIdBooks() с перебором всех ID}
-     * или {@link Book#getIDBooks() getIDBooks() через атрибут экземпляра}
+     * или {@link Book#getIDBooks() getIDBooks() через атрибут экземпляра} - {@link Book#IDBooks IDBooks}
      * @param name Название книги
      * @param author Автор
      * @param edition Издание
      * @param publisher Издатель
      * @param publication_year Год издания
-     * @param category Категоря
-     * @throws Exception Если ID уже существует вызовет исключение.
+     * @param category Категория
+     * @throws Exception Если ID уже существует, вызовет исключение.
      */
     public void addNewBook(int id, String name, String author, String edition, String publisher, int publication_year, String category) throws Exception {
         HashMap<String , String> addRow = new HashMap<>();
         addRow.put("id", Integer.toString(id));
         addRow.put("author", author);
+        addRow.put("name", name);
         addRow.put("edition", edition);
         addRow.put("publisher", publisher);
         addRow.put("publication_year", Integer.toString(publication_year));
@@ -407,15 +416,15 @@ public class Book {
     }
 
     /**
-     * Геттер названий атрибутов книги
-     * @return регистрируемые атрибуты книги
+     * Геттер атрибута {@link Book#columnsName} с названиями используемых атрибутов книги
+     * @return {@link Book#columnsName} - регистрируемые атрибуты книги
      */
     public String[] getColumnsName(){
         return columnsName;
     }
 
     /**
-     * Геттер всей информации о книгах
+     * Геттер атрибута {@link Book#allData} со всей информации о книгах
      * @return {@code ArrayList<HashMap<String, String>>} со словарями информации о книгах
      */
     public ArrayList<HashMap<String, String>> getAllDataList(){
@@ -423,8 +432,8 @@ public class Book {
     }
 
     /**
-     * Геттер ID книг
-     * @return ID всех записаных книг
+     * Геттер атрибута {@link Book#IDBooks} с ID всех книг
+     * @return ID всех записанных книг
      */
     public ArrayList<Integer> getIDBooks(){
         return IDBooks;
