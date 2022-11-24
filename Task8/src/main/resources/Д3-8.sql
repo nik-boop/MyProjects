0c70318512346f104e7df285344f837e2a4e95f4
Nikita <nakoryakin02@gmail.com>
2022-11-24 10:11:26 +0300

    Task 8

--- Task8/src/main/resources/Д3-8.sql
+++ Task8/src/main/resources/Д3-8.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`people` (
   `marital_status` TINYINT(1) NULL DEFAULT NULL,
   PRIMARY KEY (`id`));
   
-INSERT INTO `people` (`id`,`firstname`,`surname`,`date_of_birth`,`place_of_birth`,`salary`,`marital_status`) VALUES (1,'John','Do','1998-05-25','Plase_1',1234.56,1);
-INSERT INTO `people` (`id`,`firstname`,`surname`,`date_of_birth`,`place_of_birth`,`salary`,`marital_status`) VALUES (2,'Salim','Salem','1984-01-12','Morocco',6543.21,1);
-INSERT INTO `people` (`id`,`firstname`,`surname`,`date_of_birth`,`place_of_birth`,`salary`,`marital_status`) VALUES (3,'Yuri','Puturin','1980-02-29','Russia',178949,0);
-INSERT INTO `people` (`id`,`firstname`,`surname`,`date_of_birth`,`place_of_birth`,`salary`,`marital_status`) VALUES (4,'Juan','De La Cruiz','1598-04-10','Mexico',0.1,1);
+INSERT INTO `mydb`.`people` (`id`,`firstname`,`surname`,`date_of_birth`,`place_of_birth`,`salary`,`marital_status`) VALUES (1,'John','Do','1998-05-25','Plase_1',1234.56,1);
+INSERT INTO `mydb`.`people` (`id`,`firstname`,`surname`,`date_of_birth`,`place_of_birth`,`salary`,`marital_status`) VALUES (2,'Salim','Salem','1984-01-12','Morocco',6543.21,1);
+INSERT INTO `mydb`.`people` (`id`,`firstname`,`surname`,`date_of_birth`,`place_of_birth`,`salary`,`marital_status`) VALUES (3,'Yuri','Puturin','1980-02-29','Russia',178949,0);
+INSERT INTO `mydb`.`people` (`id`,`firstname`,`surname`,`date_of_birth`,`place_of_birth`,`salary`,`marital_status`) VALUES (4,'Juan','De La Cruiz','1598-04-10','Mexico',0.1,1);
