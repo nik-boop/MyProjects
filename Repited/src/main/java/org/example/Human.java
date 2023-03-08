2a545b86ee06d064e37f88325d5f67ed51db13e4
Nikita <nakoryakin02@gmail.com>
2023-03-08 22:16:18 +0300

    Lambda

--- Repited/src/main/java/org/example/Human.java
+++ Repited/src/main/java/org/example/Human.java
@@ -1,22 +1,68 @@
 package org.example;
 
-public class Human {
-    String name;
-    Integer age;
+import java.time.LocalDate;
 
-    public String getName() {
-        return name;
+public class Human {
+    int age;
+    String firstName;
+    String lastName;
+    LocalDate birthDate;
+    int weight;
+
+    public Human(int age,
+                 String firstName,
+                 String lastName,
+                 LocalDate birthDate,
+                 int weight) {
+        setAge(age);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setBirthDate(birthDate);
+        setWeight(weight);
     }
 
-    public Integer getAge() {
+    @Override
+    public String toString() {
+        return String.format("age - %s firstName - %s lastName - %s birthDate - %s weight - %s", age, firstName, lastName, birthDate, weight);
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
         return age;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
 }
--- Repited/src/main/java/org/example/Main.java
+++ Repited/src/main/java/org/example/Main.java
@@ -2,21 +2,59 @@ package org.example;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.example.Human;
 
 public class Main {
+    private static void pr(int d){
+        System.out.println(d);
+    }
+    private static void pr(String d){
+        System.out.println(d);
+    }
+    private static void pr(LocalDate d){
+        System.out.println(d.toString());
+    }
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(new FileInputStream("src/main/resources/Human"));
         var human = new ArrayList <Human>();
-        String newLine = sc.nextLine();
-        System.out.println(newLine);
-        while (!newLine.equals("")){
-            System.out.println(newLine);
+        Scanner sc = new Scanner(new FileInputStream("src/main/resources/Human"));
+        String newLine;
+        String Head = sc.nextLine();
+        String[] data;
+        String[] d2;
+        int age;
+        String firstName;
+        String lastName;
+        LocalDate birthDate;
+        int weight;
+        int Year;
+        int Month;
+        int Day;
+        while (sc.hasNext()){
             newLine = sc.nextLine();
+            data = newLine.split(",");
+            age = Integer.parseInt(data[0]);
+            firstName = data[1];
+            lastName = data[2];
+            d2 = data[3].split("/");
+            birthDate = LocalDate.of(Integer.parseInt(d2[0]), Integer.parseInt(d2[1]), Integer.parseInt(d2[2]));
+            weight = Integer.parseInt(data[4]);
+            System.out.println(age);
+            human.add(new Human(age, firstName, lastName, birthDate, weight));
         }
+        human.stream().forEach(x -> System.out.println(x.toString()));
+        System.out.println("---");
+        human.stream().sorted(Comparator.comparing(Human::getFirstName).reversed()).forEach(x -> System.out.println(x.toString()));
+        System.out.println("---");
+        human.stream().filter(x -> x.getAge() > 20).forEach(x -> System.out.println(x.toString()));
+        System.out.println("---");
+        human.stream().limit(3).forEach(x -> System.out.println(x.toString()));
+        System.out.println("---");
+        System.out.println(human.stream().map(x -> x.getFirstName()).collect(Collectors.joining(" ")));
+        System.out.println("---");
     }
 }
